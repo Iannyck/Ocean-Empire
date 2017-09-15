094e15f7f070621f4ee3bfe066774976bdb9b56d
licence dim <licences.dim@uqac.ca>
2017-09-15 13:34:00 -0400

    first push benoit

---
+++ OceanEmpire/Assets/SubmarineMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineMovement : MonoBehaviour {
+
+    public float acceleration;
+    public float maximumSpeed;
+
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("space"))
+            SetDestination(new Vector3(0, 0, 0));
+    }
+
+
+    void SetDestination(Vector3 dest)
+    {
+        rb.AddForce((transform.position - dest) * 10);
+     
+    }
+}
