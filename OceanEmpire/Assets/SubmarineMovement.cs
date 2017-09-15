6096ae77315b745462b7845f50f8115b035ba4ca
licence dim <licences.dim@uqac.ca>
2017-09-15 18:04:30 -0400

    mouvement basic du sousmarin

--- OceanEmpire/Assets/SubmarineMovement.cs
+++ OceanEmpire/Assets/SubmarineMovement.cs
@@ -1,29 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SubmarineMovement : MonoBehaviour {
+public class SubmarineMovement : MonoBehaviour, Interfaces.IClickInputs {
 
-    public float acceleration;
+        //Proportionnal to the acceleration rate
+    public float accelerationRate;
+        //Maximium attainable speed
     public float maximumSpeed;
+        //Between 0 and 
+    public float brakeRatio = 0.5F;
+    private float moveAccuracy = 1.0F;
+
+    private Vector2 currentTarget;
 
     public Rigidbody2D rb;
 
     // Use this for initialization
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        currentTarget = new Vector2(transform.position.x, transform.position.y);
     }
 
+
+    void FixedUpdate()
+    {
+       
+        Vector2 distance = currentTarget - new Vector2(transform.position.x, transform.position.y);
+        Vector2 direction = distance.normalized;
+
+
+        if (distance.magnitude < 4)
+        {
+            
+            Debug.Log(rb.velocity.magnitude);
+            /*
+                        Vector2 deceleration = -rb.velocity;
+
+                        if (rb.velocity.sqrMagnitude > 1)
+                            deceleration = (-rb.velocity).normalized;
+
+                        rb.AddForce(deceleration * accelerationRate);
+                        */
+        }
+
+        else if (Vector2.Dot(rb.velocity, direction) < maximumSpeed)
+        {
+
+
+            Vector2 perpSpeed = rb.velocity - Vector2.Dot(rb.velocity, direction) * direction;
+            float perpSpeedPourcent = (perpSpeed.magnitude / maximumSpeed) * brakeRatio;
+
+            Vector2 thrustersForce = -perpSpeed.normalized * perpSpeedPourcent + direction * (1 - perpSpeedPourcent.Capped(1)) * accelerationRate;
+            rb.AddForce(thrustersForce);
+
+            rb.AddForce(direction * accelerationRate);
+
+
+        }
+    }
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown("space"))
-            SetDestination(new Vector3(0, 0, 0));
+
+       
     }
 
 
-    void SetDestination(Vector3 dest)
+    public void OnClick(Vector2 position)
     {
-        rb.AddForce((transform.position - dest) * 10);
-     
+        currentTarget = position;
     }
 }
