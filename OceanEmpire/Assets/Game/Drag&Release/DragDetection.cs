09847699a1f672835a598962fbcb4dd67b7ec3e1
Fred UQAC <msfredb7@gmail.com>
2017-09-15 14:23:34 -0400

    Drag and release

--- OceanEmpire/Assets/Game/Drag&Release/DragDetection.cs
+++ OceanEmpire/Assets/Game/Drag&Release/DragDetection.cs
@@ -1,16 +1,120 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class DragDetection : MonoBehaviour {
+public class DragDetection : MonoBehaviour
+{
+    [System.Serializable]
+    public class DragEvent: UnityEvent<Vector2> { }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float dragMinScreenPercentage = 0.01f;
+    public DragEvent onStartDrag = new DragEvent();
+    public DragEvent onReleaseDrag = new DragEvent();
+
+    [ReadOnly]
+    public float dragMinSQRDist = 5;
+
+    [ReadOnly]
+    public DragProfile drag = DragProfile.Null;
+
+    private void Awake()
+    {
+        dragMinSQRDist = Screen.height * dragMinScreenPercentage;
+        dragMinSQRDist *= dragMinSQRDist;
+    }
+
+    void Update()
+    {
+        Vector2 touchPos;
+        bool isTouching = GetTouchPosition(out touchPos);
+
+        if (isTouching)
+        {
+            if (drag.state == DragProfile.State.Released)
+            {
+                drag = new DragProfile(touchPos);
+            }
+            else
+            {
+                drag.UpdateState(touchPos, dragMinSQRDist);
+            }
+        }
+        else
+        {
+            drag.UpdateState();
+        }
+    }
+
+    public static bool GetTouchPosition(out Vector2 position)
+    {
+        bool touching = false;
+        if (Input.touchSupported)
+        {
+            touching = Input.touchCount > 0;
+            if (touching)
+                position = Input.GetTouch(0).position;
+            else
+                position = Vector2.zero;
+        }
+        else
+        {
+            touching = Input.GetMouseButton(0);
+            if (touching)
+                position = Input.mousePosition;
+            else
+                position = Vector2.zero;
+        }
+        return touching;
+    }
+}
+
+[System.Serializable]
+public struct DragProfile
+{
+    public Vector2 startPosition;
+    public Vector2 currentPosition;
+    public enum State { Pressed = 0, Dragged = 1, Released = 2 }
+    public State state;
+
+    public DragProfile(Vector2 touch)
+    {
+        startPosition = touch;
+        currentPosition = startPosition;
+        state = State.Pressed;
+    }
+
+    public void UpdateState(Vector2 touch, float dragMinSQRDist)
+    {
+        switch (state)
+        {
+            case State.Pressed:
+                if ((currentPosition - startPosition).sqrMagnitude > dragMinSQRDist)
+                {
+                    state = State.Dragged;
+                }
+                break;
+            case State.Released:
+                startPosition = touch;
+                state = State.Pressed;
+                break;
+        }
+        currentPosition = touch;
+    }
+
+    public void UpdateState()
+    {
+        state = State.Released;
+    }
+
+    public static DragProfile Null
+    {
+        get
+        {
+            return new DragProfile()
+            {
+                state = State.Released
+            };
+        }
+    }
 }
--- OceanEmpire/Assets/TestScript.cs
+++ OceanEmpire/Assets/TestScript.cs
@@ -2,27 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FullInspector;
 
-public class TestScript : MonoBehaviour
+public class TestScript : BaseBehavior
 {
-    [System.Serializable]
-    public struct A : IWeight
-    {
-        public string value;
-        public float weight;
-        public float Weight { get { return weight; } }
-    }
-    public A[] elements;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            Debug.Log("TesT");
-        }
-    }
 }
-
-
-//trying out stuff
