950e64ced2e33862385995ce5c5b14974285a933
alex534488 <alexandre-blanchet94@hotmail.com>
2017-09-22 10:49:57 -0400

    Push du Master Manager

---
+++ OceanEmpire/Assets/Game/Recolte/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public int score; // cash
+    public float depthRecord;
+    public List<BaseFish> fishInStock = new List<BaseFish>();
+
+    void Start ()
+    {
+        score = 0;
+        // load depthRecord for this level
+    }
+
+    public void AddFishToStock(BaseFish fish)
+    {
+        fishInStock.Add(fish);
+        // Add score
+    }
+}
