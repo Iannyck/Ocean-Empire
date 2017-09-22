1b434787f572af0ab8f8c6095f836ebf084fae41
Fred UQAC <msfredb7@gmail.com>
2017-09-22 18:27:41 -0400

    Bobo le poisson tourne

--- OceanEmpire/Assets/Game/Units/SpriteFlipper.cs
+++ OceanEmpire/Assets/Game/Units/SpriteFlipper.cs
@@ -1,25 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteFlipper : MonoBehaviour
 {
+    private const float REPOS = 0.1f;
+
     public bool facingRight;
     public Rigidbody2D rb;
+    public float flipDuration = 0.4f;
+    public Ease flipEase = Ease.InOutSine;
 
     private SpriteRenderer sprRenderer;
+    private Tween flipTween;
+    private float scale;
 
     private void Awake()
     {
         sprRenderer = GetComponent<SpriteRenderer>();
+        scale = transform.localScale.x;
+        if (!facingRight)
+            scale = -scale;
     }
 
     private void Update()
     {
-        if(rb != null)
+        if (rb != null)
         {
-
+            if (facingRight)
+            {
+                if(rb.velocity.x < -REPOS)
+                {
+                    facingRight = false;
+                    Flip(-scale);
+                }
+            }
+            else
+            {
+                if (rb.velocity.x > REPOS)
+                {
+                    facingRight = true;
+                    Flip(scale);
+                }
+            }
         }
+    }
+
+    void Flip(float scale)
+    {
+        if (flipTween != null && flipTween.IsActive())
+            flipTween.Kill();
+        flipTween = transform.DOScaleX(scale, flipDuration).SetEase(flipEase);
     }
 }
