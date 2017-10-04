899b165535a144a6d0f3504e98f37e4c10ae0f60
DESKTOP-A46VL7R\etata <fivocx@gmail.com>
2017-10-04 18:45:39 -0400

    Spawn Palier de poisson

---
+++ OceanEmpire/Assets/Game/Recolte/FishSpawner/FishPalier.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishPalier{
+
+    public event SimpleEvent palierDespawnEvent;
+    public bool isActive = false;
+
+    public static float repopulationCycle;
+    private float lastActivationTime;
+
+    private float AbsoluteFishLimit;
+    private float currentFishLimit;
+
+    public void Init(float limit)
+    {
+        currentFishLimit = AbsoluteFishLimit = limit;
+        lastActivationTime = 0;
+    }
+
+    public void UpdateActivationTime(float aTime)
+    {
+        lastActivationTime = aTime;
+    }
+
+    public void Despawn(float time)
+    {
+        "Called".Log();
+        if (palierDespawnEvent != null)
+            palierDespawnEvent.Invoke();
+
+        lastActivationTime = time;
+     
+        /*
+        float count = currentFishes.Count;
+        for (int i = 0; i < count; i++)
+        { 
+            currentFishes[i].Kill();
+        }
+        currentFishes.Clear();
+        */
+    }
+     
+    public void SuscribeFish(BaseFish fish)
+    {
+        fish.captureEvent += CaptureFish;
+    }
+
+    public void UnSuscribeFish(BaseFish fish)
+    {
+        fish.captureEvent -= CaptureFish;
+    }
+
+
+
+
+    public float GetFishDensity(float currentTime)
+    {
+        float regenRate = ((currentTime - lastActivationTime) / repopulationCycle).Capped(1.0f);
+        int additionnalFishes = (int)(AbsoluteFishLimit * regenRate).Raised(0.0f);
+        currentFishLimit = (currentFishLimit + additionnalFishes).Capped(AbsoluteFishLimit);
+        return currentFishLimit;
+    }
+
+
+
+    public void CaptureFish(BaseFish fish)
+    {
+        currentFishLimit -= (currentFishLimit > 0 ? 1 : 0);
+    }
+}
