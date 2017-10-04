f00f897e435927409bc2a0e5ed243ac7f19f626f
alex534488 <alexandre-blanchet94@hotmail.com>
2017-10-04 17:44:39 -0400

    Ajout de largent

---
+++ OceanEmpire/Assets/Game/Managers/PlayerCurrency.cs
@@ -0,0 +1,70 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerCurrency : BaseManager<PlayerCurrency>
+{
+    private const string SAVE_KEY_MONEY = "money";
+
+    public override void Init()
+    {
+        Money = GameSaves.instance.GetInt(GameSaves.Type.Shop, SAVE_KEY_MONEY);
+    }
+
+    private int money;
+
+    public static int Money
+    {
+        private set
+        {
+            instance.money = value;
+            Save();
+        }
+
+        get
+        {
+            return instance.money;
+        }
+    }
+
+    public static int Add
+    {
+        set
+        {
+            instance.add(value);
+        }
+    }
+
+    public static int Substract
+    {
+        set
+        {
+            instance.remove(value);
+        }
+    }
+
+    public void remove(int amount)
+    {
+        if ((Money - amount) >= 0)
+        {
+            Money = 0;
+            return;
+        }
+        Money -= amount;
+    }
+
+    public void add(int amount)
+    {
+        if (amount <= 0)
+            return;
+        Money += amount;
+    }
+
+    private static void Save()
+    {
+        GameSaves.instance.SetInt(GameSaves.Type.Shop, SAVE_KEY_MONEY, Money);
+        GameSaves.instance.SaveData(GameSaves.Type.Shop);
+    }
+}
---
+++ OceanEmpire/Assets/Game/UI/Shack/MoneyDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyDisplay : MonoBehaviour {
+
+    public Text text;
+
+    private string startDisplay;
+
+	void Start ()
+    {
+        if(text != null)
+            startDisplay = text.text;
+    }
+	
+	void Update ()
+    {
+		if(PlayerCurrency.instance != null)
+            text.text = startDisplay + PlayerCurrency.Money + "$";
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            PlayerCurrency.Add = 1;
+        }
+	}
+}
