647aa124de67618c27ce9a67aef6ad64df5bf4f2
Fred UQAC <msfredb7@gmail.com>
2017-10-14 16:03:24 -0400

    Exercice Panel

---
+++ OceanEmpire/Assets/Game/UI/ExercicePanel/ExercicePanel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExercicePanel : MonoBehaviour
+{
+    public const string SCENENAME = "ExercicePanel";
+}
