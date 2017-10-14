072d4b10f998c74444e388c00617ffc03aa80ffe
alex534488 <alexandre-blanchet94@hotmail.com>
2017-10-14 18:04:47 -0400

    Merge Du build

--- OceanEmpire/Assets/FullInspector2_Generated/PropertyDrawerIntegration/Generated_UnityEngine_Events_UnityEvent_MonoBehaviourStorage.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using FullInspector.Internal;
-using UnityEngine;
-
-namespace FullInspector.Generated {
-    [AddComponentMenu("")]
-    public class Generated_UnityEngine_Events_UnityEvent_MonoBehaviourStorage : fiPropertyDrawerMonoBehaviorContainer<UnityEngine.Events.UnityEvent> {} 
-}
