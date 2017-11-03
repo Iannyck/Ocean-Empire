bc1f08b1d14a3cae1d2a31f8497713ad9f753cf2
alex534488 <alexandre-blanchet94@hotmail.com>
2017-11-03 17:19:58 -0400

    Premire iteration du tracker

--- OceanEmpire/Assets/Game/Android/LocalNotification.cs
+++ OceanEmpire/Assets/Game/Android/LocalNotification.cs
@@ -29,7 +29,7 @@ public class LocalNotification
         {
             pluginClass.CallStatic("SetNotification", id, delayMs, title, message, message, 
                 sound ? 1 : 0, vibrate ? 1 : 0, lights ? 1 : 0, bigIcon, "notify_icon_small", 
-        bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, Application.bundleIdentifier);
+        bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, Application.identifier);
         }
         return id;
         #else
@@ -50,16 +50,16 @@ public class LocalNotification
 
     public static int SendRepeatingNotification(int id, long delayMs, long timeoutMs, string title, string message, Color32 bgColor, bool sound = true, bool vibrate = true, bool lights = true, string bigIcon = "")
     {
-        #if UNITY_ANDROID && !UNITY_EDITOR
+#if UNITY_ANDROID && !UNITY_EDITOR
         AndroidJavaClass pluginClass = new AndroidJavaClass(fullClassName);
         if (pluginClass != null)
         {
             pluginClass.CallStatic("SetRepeatingNotification", id, delayMs, title, message, message, timeoutMs, 
                 sound ? 1 : 0, vibrate ? 1 : 0, lights ? 1 : 0, bigIcon, "notify_icon_small", 
-                bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, Application.bundleIdentifier);
+                bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, Application.identifier);
         }
         return id;
-        #else
+#else
         return 0;
         #endif
     }
---
+++ OceanEmpire/Assets/Game/Android/MessageReceiver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CCC.Manager;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MessageReceiver : BaseManager<MessageReceiver> {
+
+    public const string SCENE_NAME = "DebugMessage";
+
+    public override void Init()
+    {
+        JavaMessage(GetAndroidMessage());
+    }
+
+    public void JavaMessage(string message)
+    {
+        Scenes.LoadAsync(SCENE_NAME, LoadSceneMode.Additive, delegate (Scene scene) {
+            scene.FindRootObject<DebugMessageAffichage>().SetText(message);
+            DelayManager.LocalCallTo(delegate ()
+            {
+                Scenes.UnloadAsync(SCENE_NAME);
+            }, 1, this);
+        });
+    }
+
+    public static string GetAndroidMessage()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        AndroidJavaClass myClass = new AndroidJavaClass("com.UQAC.OceanEmpire.ActivityDetection");
+        return myClass.Call<string>("GetCurrentState", new object[] { });
+#else
+        return "";
+#endif
+    }
+}
