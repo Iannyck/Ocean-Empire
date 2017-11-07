c6503435c96fd637bab01abc7dd7529b778f31e6
Fred UQAC <msfredb7@gmail.com>
2017-11-07 17:49:24 -0500

    Début du calendrier scroll

---
+++ OceanEmpire/Assets/Game/Debug/Fred/CalendarDisplay/Scroll/CalendarScroll_Scroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class CalendarScroll_Scroller : MonoBehaviour
+{
+    public ScrollRect scroller;
+
+    private void Start()
+    {
+        scroller.onValueChanged.AddListener(OnScrollValueChanged);
+    }
+
+    void OnScrollValueChanged(Vector2 normalizedPosition)
+    {
+        print(normalizedPosition);
+        if(normalizedPosition.y < -1)
+        {
+            scroller.verticalNormalizedPosition = 0;
+            //scroller.velocity
+            Debug.LogWarning("sup");
+        }
+    }
+
+    public void NormalizedPositionToZero()
+    {
+        scroller.verticalNormalizedPosition = 0;
+    }
+}
