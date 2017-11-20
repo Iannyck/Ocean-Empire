c28b0c5cc1c2977ab682bd2ffbe2e9448ef52b4e
Fred UQAC <msfredb7@gmail.com>
2017-11-20 17:04:37 -0500

    Backbone des rewards

---
+++ OceanEmpire/Assets/Game/Exercice Backend/Rewards/Reward.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Reward
+{
+    public abstract void Apply();
+    public abstract RewardType GetRewardType();
+    public abstract float GetValue();
+}
---
+++ OceanEmpire/Assets/Game/Exercice Backend/Rewards/RewardTypes/Reward_Coins.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reward_Coins : Reward
+{
+    public int amount;
+
+    public override void Apply()
+    {
+    }
+
+    public override float GetValue()
+    {
+        return amount * RewardComponents.GetBaseValue(GetRewardType());
+    }
+
+    private Reward_Coins(int coins)
+    {
+        amount = coins;
+    }
+
+    public override RewardType GetRewardType()
+    {
+        return RewardType.Coins;
+    }
+
+    public Reward_Coins BuildReverse(int value)
+    {
+        return new Reward_Coins(Mathf.RoundToInt(value / RewardComponents.GetBaseValue(RewardType.Coins)));
+    }
+
+    public Reward_Coins Build(int coinAmount)
+    {
+        return new Reward_Coins(coinAmount);
+    }
+}
