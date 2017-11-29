e7ad653f76f91140fe020799194adbdc329c274c
Xeal <alexandre.blanchet.94@gmail.com>
2017-11-28 20:01:12 -0500

    Fix de la tracking window

---
+++ OceanEmpire/Assets/Game/InGame Systems/Exercice/Windows/Tracking/WaitAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WaitAnimation : MonoBehaviour {
+
+    public float animDuration = 2;
+	
+	public void DoAnimation ()
+    {
+        gameObject.transform.DORotate(new Vector3(0, 0, -180), animDuration).SetLoops(-1,LoopType.Incremental).SetUpdate(true);
+	}
+}
