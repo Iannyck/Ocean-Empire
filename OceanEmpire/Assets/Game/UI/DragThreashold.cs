5039ab134fa725405a0e17553f777cb588496f69
Fred UQAC <msfredb7@gmail.com>
2017-11-30 00:37:49 -0500

    DragThreshhold (pas encore fini)

---
+++ OceanEmpire/Assets/Game/UI/DragThreashold.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragThreashold : PublicSingleton<DragThreashold>
+{
+    public EventSystem eventSystem;
+
+    public float inMenuDragSize = 0.01f;
+    public float inGameDragSize = 0.001f;
+
+    public enum DragType { InGame = 0, InMenu = 1 }
+
+    public void SetDragType(DragType type)
+    {
+        int pixels = 0;
+        float size = 0;
+        switch (type)
+        {
+            case DragType.InGame:
+                size = inGameDragSize;
+                break;
+            case DragType.InMenu:
+                size = inMenuDragSize;
+                break;
+        }
+        pixels = (size * Screen.width).RoundedToInt();
+        eventSystem.pixelDragThreshold = pixels;
+    }
+}
