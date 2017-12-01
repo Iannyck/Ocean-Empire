ed600944a0308aa011b91edf66198eb53358e99d
Benoit <licences.dim@uqac.ca>
2017-11-30 21:19:50 -0500

    Nouveau poissons integré dans la maps (+ chest)

---
+++ OceanEmpire/Assets/Game/Units/Poisson/Common Classes/Chest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : BaseFish {
+
+    public void OnPlayerTouch()
+    {
+        Game.PlayerStats.TryCapture(this);
+        //Capture();
+    }
+}
