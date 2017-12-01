f6df2dce28a91b93893fe3e153bde6563a9a4eeb
alex534488 <alexandre-blanchet94@hotmail.com>
2017-12-01 02:40:13 -0500

    fix ui recolte

--- OceanEmpire/Assets/Game/Recolte/UI/GazSlider.cs
+++ OceanEmpire/Assets/Game/Recolte/UI/GazSlider.cs
@@ -7,6 +7,9 @@ using DG.Tweening;
 public class GazSlider : MonoBehaviour {
 
     public Slider gageMeter;
+    public Image bg;
+
+    private bool startAlert = false;
 
     void Start()
     {
@@ -16,6 +19,10 @@ public class GazSlider : MonoBehaviour {
     void Update()
     {
         UpdateMeter();
+        if(gageMeter.value < 0.2f)
+        {
+            Alerte();
+        }
     }
 
     public void UpdateMeter()
@@ -30,5 +37,14 @@ public class GazSlider : MonoBehaviour {
         { 
             gageMeter.value = 1;
         }
+    }
+
+    public void Alerte()
+    {
+        if (startAlert)
+            return;
+
+        startAlert = true;
+        bg.DOColor(Color.red, 0.5f).SetLoops(-1, LoopType.Yoyo);
     }
 }
