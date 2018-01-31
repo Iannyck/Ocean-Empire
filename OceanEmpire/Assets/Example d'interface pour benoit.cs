a71a2bd5a226f871ef3fcfa7ec9402414e5953e6
Fred UQAC p3-5050 <w@gmail.com>
2018-01-30 22:02:39 -0500

    Exampl d'interface pour benoit

---
+++ OceanEmpire/Assets/Example d'interface pour benoit.cs
@@ -0,0 +1,47 @@
+﻿/*
+using System;
+using System.Reflection;
+using UnityEngine;
+
+public abstract class TestScript : MonoBehaviour, IShopDisplayable, IDescription
+{
+    public abstract bool Buy(CurrencyType type);
+
+    public string GetDescription()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Sprite GetIcon()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetPrice(CurrencyType type)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetTitle()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IShopDisplayable : IBuyable
+{
+    Sprite GetIcon();
+    string GetTitle();
+}
+
+public interface IBuyable
+{
+    bool Buy(CurrencyType type);
+    int GetPrice(CurrencyType type);
+}
+
+public interface IDescription
+{
+    string GetDescription();
+}
+*/
