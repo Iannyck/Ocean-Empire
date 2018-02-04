e1e1ea7940e49c0e9a0bc9ed4e8d44dfd4580a14
Fred Bessette <msfredb7@gmail.com>
2018-02-03 19:47:51 -0500

    I deleted the Preferences class as it is of no use for now. It was originally designed for the now unsupported task system.

--- OceanEmpire/Assets/Game/Scripts/Exercice Backend/PlayerProfile/PlayerProfile.cs
+++ OceanEmpire/Assets/Game/Scripts/Exercice Backend/PlayerProfile/PlayerProfile.cs
@@ -4,19 +4,10 @@ using System;
 
 public class PlayerProfile : MonoPersistent
 {
-    private const string SAVE_KEY_PREFERENCE = "preference";
     private const string SAVE_KEY_LEVEL = "level";
 
-    //public static int Level
-    //{
-    //    get { return instance.level; }
-    //    set { instance.level = value; instance.SavePlayerProfil(); }
-    //}
-
-    //[SerializeField, ReadOnly] private int level;
     [SerializeField] private DataSaver playerProfileSaver;
 
-    public Preferences preferences;
     public static PlayerProfile instance;
 
     protected void Awake()
@@ -31,38 +22,17 @@ public class PlayerProfile : MonoPersistent
         onComplete();
     }
 
-    //public static void IncrementLevel(int value)
-    //{
-    //    Level = (Level + value).Capped(taskDifficulty.MaxLevel);
-    //}
-
-    //public static void DecrementLevel(int value)
-    //{
-    //    Level = (Level - value).Raised(0);
-    //}
-
 
     private void SavePlayerProfil()
     {
-        playerProfileSaver.SetObjectClone(SAVE_KEY_PREFERENCE, preferences);
-        //playerProfileSaver.SetInt(SAVE_KEY_LEVEL, level);
-        playerProfileSaver.Save();
+        //Set data into playerProfileSaver
+        //playerProfileSaver.Save();
     }
 
     private void FetchPlayerProfil()
     {
-        preferences = playerProfileSaver.GetObjectClone(SAVE_KEY_PREFERENCE) as Preferences;
-        //level = playerProfileSaver.GetInt(SAVE_KEY_LEVEL, 0);
+        //Get data from playerProfileSaver
     }
-
-    //public static void UpdatePlayerLevel(Task completedTask)
-    //{
-    //    if (taskDifficulty.GetTaskLevel(completedTask) < Level)
-    //        DecrementLevel(1);
-    //    else if (taskDifficulty.GetTaskLevel(completedTask) > Level)
-    //        IncrementLevel(1);
-    //    return;
-    //}
 
     public static void Refetch()
     {
