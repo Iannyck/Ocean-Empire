396f7fabdb77bf30cc3e07fc7130e2b222cb4f7a
Ordinateur2-Local <alexandre.blanchet.94@gmail.com>
2018-02-04 17:20:26 -0500

    Ajout du UI pour la fenetre de completion ou de fail d'une zone bonus

---
+++ OceanEmpire/Assets/Game/UI/Windows/Completion/CompletionWindow.cs
@@ -0,0 +1,34 @@
+﻿using CCC.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompletionWindow : WindowAnimation {
+
+    public class Rewards
+    {
+        public int amount;
+        public Sprite icon;
+    }
+
+    public int differentRewardMax = 4;
+    public RewardDisplay rewardPrefab;
+    public Transform countainer;
+    public Button exitButton;
+
+	public void ShowCompletionRewards(List<Rewards> rewards)
+    {
+        exitButton.onClick.AddListener(delegate ()
+        {
+            Close();
+        });
+        if (rewards.Count >= differentRewardMax)
+            return;
+        for (int i = 0; i < rewards.Count; i++)
+        {
+            Instantiate(rewardPrefab, countainer).GetComponent<RewardDisplay>().UpdateRewardInfo(rewards[i].amount, rewards[i].icon);
+        }
+        Open();
+    }
+}
---
+++ OceanEmpire/Assets/Game/UI/Windows/Failure/FailureAsk.cs
@@ -0,0 +1,56 @@
+﻿using CCC.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FailureAsk : MonoBehaviour {
+
+    private int maxChoiceAmount = 4;
+
+    public ChoiceButton buttonPrefab;
+    public Transform countainer;
+
+    public WindowAnimation adviceWindow;
+
+    [Serializable]
+    public struct Choice
+    {
+        public string choiceText;
+        public string advice;
+    }
+ 
+    public List<Choice> choixPossible = new List<Choice>();
+
+	void Start ()
+    {
+        for (int i = 0; i < choixPossible.Count; i++)
+        {
+            ChoiceButton currentButton = Instantiate(buttonPrefab, countainer).GetComponent<ChoiceButton>();
+            if (currentButton == null)
+                Debug.Log("ChoiceButton Inexistant lors de sa creation");
+            currentButton.UpdateButtonInfo(DisplayAdvice, choixPossible[i].choiceText, choixPossible[i].advice);
+        }
+	}
+
+    void DisplayAdvice(string advice)
+    {
+        WindowAnimation askWindowAnim = GetComponent<WindowAnimation>();
+        if(askWindowAnim == null)
+            Debug.Log("Pas de window anim sur la fenetre ASK");
+        askWindowAnim.Close(delegate() {
+            adviceWindow.GetComponent<FailureAdvice>().UpdateInfo(advice);
+            adviceWindow.Open();
+        });
+    }
+
+    private void OnValidate()
+    {
+        if(choixPossible.Count >= maxChoiceAmount)
+        {
+            Debug.Log("Trop de choix dans : FailureAsk");
+            choixPossible.RemoveRange(4, Mathf.Max(choixPossible.Count-4,0));
+        }
+    }
+}
