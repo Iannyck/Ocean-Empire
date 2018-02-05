b5d897f7177c9ecde3c274107d860af918531d10
Fred UQAC p3-5050 <w@gmail.com>
2018-02-04 21:44:42 -0500

    remove des warnings de unity

--- OceanEmpire/Assets/Game/Scripts/Upgrade/Harpoon Thrower/HarpoonThrowerDescription.cs
+++ OceanEmpire/Assets/Game/Scripts/Upgrade/Harpoon Thrower/HarpoonThrowerDescription.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Ocean Empire/Shop/Upgrade Description/Harpoon Thrower Description")]
 public class HarpoonThrowerDescription : UpgradeDescription {
     [SerializeField, ReadOnly] private HarpoonThrower harpoonThrower;
     public HarpoonThrower GetHarpoonThrower() { return harpoonThrower; }
--- OceanEmpire/Assets/Game/Scripts/Upgrade/Thruster/ThrusterDescription.cs
+++ OceanEmpire/Assets/Game/Scripts/Upgrade/Thruster/ThrusterDescription.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Ocean Empire/Shop/Upgrade Description/Thruster Description")]
 public class ThrusterDescription : UpgradeDescription {
     [SerializeField, ReadOnly] private Thruster thruster;
     public Thruster GetThruster() { return thruster;}
