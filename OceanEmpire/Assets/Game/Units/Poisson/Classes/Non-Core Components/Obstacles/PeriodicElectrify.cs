69cba29668a4b68011a8e9bb176a8c47b9b01079
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2018-02-16 18:04:53 -0500

    Ajout de musique dans le jeu + Changement au electrify pour etre plus modulaire. L'aspect periodique est maintenant dans un autre script PeriodicElectrify

---
+++ OceanEmpire/Assets/Game/Units/Poisson/Classes/Non-Core Components/Obstacles/PeriodicElectrify.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeriodicElectrify : Electrify
+{
+    public float electricInterval = 2.5f;
+    public float electricDuration = 2.5f;
+
+
+    protected override void Start () {
+        base.Start();
+
+        this.DelayedCall(ElectricRestart, electricDuration);
+    }
+
+    void ElectricRestart()
+    {
+        StopElectricEffect();
+        this.DelayedCall(delegate() {
+            ElectricEffect();
+            this.DelayedCall(ElectricRestart, electricDuration);
+        }, electricInterval);
+    }
+}
