9b9733b67e496a334786bc41e96fe19750edd679
Xeal <alexandre-blanchet94@hotmail.com>
2018-02-18 23:33:23 -0500

    Ajout dans CCC du popup. Parce qu'on l'a utilisé trop souvent dans plein de jeu.

---
+++ OceanEmpire/Assets/Game/Debug/Alex/AlexTestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using CCC.UI.Animation;
+
+public class AlexTestScript : MonoBehaviour {
+
+    public Popup popup;
+    public Sprite image;
+    public string text = "LOL";
+    public bool useOutLine = true;
+    public Vector2 anchorMoveDelta = new Vector2(0, -200);
+    public Vector3 imageScale = new Vector3(0.5f,0.5f,0.5f);
+    public int textSize;
+    public Color color;
+    public Font font;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            Popup.CreatePopup(popup,transform).ChangeAnimationSettings(anchorMoveDelta).ChangeImageSettings(imageScale, color).Animate(gameObject,image);
+        else if (Input.GetKeyDown(KeyCode.Y))
+            Popup.CreatePopup(popup, transform).ChangeAnimationSettings(anchorMoveDelta).ChangeTextSettings(textSize, color, font).Animate(gameObject, text, useOutLine);
+    }
+}
--- OceanEmpire/Assets/Plugins/CCC/Animation/FloatingAnimation.cs
+++ OceanEmpire/Assets/Plugins/CCC/Animation/FloatingAnimation.cs
@@ -3,64 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class FloatingAnimation : MonoBehaviour
+namespace CCC.UI.Animation
 {
-    [Header("Settings")]
-    public bool animateOnEnable = true;
-    public bool stopOnDisable = true;
-    public bool independantUpdate = true;
-
-    [Header("Animation")]
-    public float cycleDuration = 1;
-    public float maxSize = 1.15f;
-    public float minSize = 0.85f;
-    public Ease ease = Ease.InOutSine;
-
-    private Tween tween;
-    private Transform tr;
-    private float normalScale = 1;
-
-    void Awake()
+    public class FloatingAnimation : MonoBehaviour
     {
-        tr = transform;
-        normalScale = tr.localScale.x;
-    }
+        [Header("Settings")]
+        public bool animateOnEnable = true;
+        public bool stopOnDisable = true;
+        public bool independantUpdate = true;
 
-    void OnEnable()
-    {
-        if (animateOnEnable)
-            Animate();
-    }
+        [Header("Animation")]
+        public float cycleDuration = 1;
+        public float maxSize = 1.15f;
+        public float minSize = 0.85f;
+        public Ease ease = Ease.InOutSine;
 
-    void OnDisable()
-    {
-        if (stopOnDisable)
-            Stop();
-    }
+        private Tween tween;
+        private Transform tr;
+        private float normalScale = 1;
 
-    public void Animate()
-    {
-        KillTween();
+        void Awake()
+        {
+            tr = transform;
+            normalScale = tr.localScale.x;
+        }
 
-        tr.localScale = Vector3.one * minSize;
+        void OnEnable()
+        {
+            if (animateOnEnable)
+                Animate();
+        }
 
-        float duration = Mathf.Max(0.02f, cycleDuration / 2);
-        tween = tr.DOScale(maxSize, duration)
-            .SetEase(ease)
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetUpdate(independantUpdate);
-    }
+        void OnDisable()
+        {
+            if (stopOnDisable)
+                Stop();
+        }
 
-    public void Stop()
-    {
-        KillTween();
-        tr.localScale = Vector3.one * normalScale;
-    }
+        public void Animate()
+        {
+            KillTween();
 
-    void KillTween()
-    {
-        if (tween != null && tween.IsActive())
-            tween.Kill();
-        tween = null;
+            tr.localScale = Vector3.one * minSize;
+
+            float duration = Mathf.Max(0.02f, cycleDuration / 2);
+            tween = tr.DOScale(maxSize, duration)
+                .SetEase(ease)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetUpdate(independantUpdate);
+        }
+
+        public void Stop()
+        {
+            KillTween();
+            tr.localScale = Vector3.one * normalScale;
+        }
+
+        void KillTween()
+        {
+            if (tween != null && tween.IsActive())
+                tween.Kill();
+            tween = null;
+        }
     }
 }
+
