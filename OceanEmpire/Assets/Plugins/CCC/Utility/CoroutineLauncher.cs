07017dc36690e9c3597b1dc2ed254b9168d3b806
Frédéric Bessette <msfredb7@gmail.com>
2018-02-21 13:23:18 -0500

    Added Coroutine Launcher
    useful to launch coroutine from non-monobehaviour objects

---
+++ OceanEmpire/Assets/Plugins/CCC/Utility/CoroutineLauncher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CoroutineLauncher : MonoBehaviour
+{
+    private static  CoroutineLauncher instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static CoroutineLauncher Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                var obj = new GameObject("Couroutine Launcher");
+                DontDestroyOnLoad(obj);
+                obj.AddComponent<CoroutineLauncher>();
+            }
+            return instance;
+        }
+    }
+}
