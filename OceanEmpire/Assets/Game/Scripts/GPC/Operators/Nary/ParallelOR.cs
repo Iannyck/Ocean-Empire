4aed9e5834667bb230164da06a59157aa4e120dd
Frédéric Bessette <msfredb7@gmail.com>
2018-02-23 18:58:50 -0500

    Fixed a bug in ParalleOR where it would fail to correctly remove children

--- OceanEmpire/Assets/Game/Scripts/GPC/Operators/Nary/ParallelOR.cs
+++ OceanEmpire/Assets/Game/Scripts/GPC/Operators/Nary/ParallelOR.cs
@@ -4,51 +4,56 @@ using UnityEngine;
 
 namespace GPComponents
 {
-	public class ParallelOR : Nary
-	{
+    public class ParallelOR : Nary
+    {
+        public override GPCState Eval()
+        {
+            GPCState state;
+            for (int i = 0; i < children.Count; i++)
+            {
+                state = children[i].Eval();
+                if (state == GPCState.FAILURE)
+                {
+                    //It is important to decrement i if we don't want to skip the next element.
+                    children.RemoveAt(i);
+                    i--;
+                }
+                else if (state == GPCState.SUCCESS)
+                {
+                    return GPCState.SUCCESS;
+                }
+            }
 
-		private List<IGPComponent> childrenCopy;
+            if (children.Count == 0)
+            {
+                return GPCState.FAILURE;
+            }
+            return GPCState.RUNNING;
+        }
 
-		public override GPCState Eval ()
-		{
+        public override void Launch()
+        {
+            foreach (IGPComponent gComponent in children)
+            {
+                gComponent.Launch();
+            }
+        }
 
-			GPCState state;
-			foreach (IGPComponent gComponent in children) {
-				state = gComponent.Eval ();
-				if (state == GPCState.FAILURE) {
-					childrenCopy.Remove (gComponent);
-				} else if (state == GPCState.SUCCESS) {
-					return GPCState.SUCCESS;
-				}
-			}
-			if (childrenCopy.Count == 0) {
-				return GPCState.FAILURE;
-			}
-			return GPCState.RUNNING;
-		}
+        public override void Reset()
+        {
+            foreach (IGPComponent gComponent in children)
+            {
+                gComponent.Reset();
+            }
+        }
 
-		public override void Launch ()
-		{
-			foreach (IGPComponent gComponent in children) {
-				gComponent.Launch ();
-			}
-			childrenCopy = children;
-		}
+        public override void Abort()
+        {
+            foreach (IGPComponent gComponent in children)
+            {
+                gComponent.Abort();
+            }
+        }
 
-		public override void Reset ()
-		{
-			foreach (IGPComponent gComponent in children) {
-				gComponent.Reset ();
-			}
-			childrenCopy = children;
-		}
-
-		public override void Abort ()
-		{
-			foreach (IGPComponent gComponent in children) {
-				gComponent.Abort ();
-			}
-		}
-
-	}
+    }
 }
