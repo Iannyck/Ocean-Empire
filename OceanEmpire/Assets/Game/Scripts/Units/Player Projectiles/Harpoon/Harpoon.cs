2cec70ed1abfb215ff1674a30efd13a935f08018
Frédéric Bessette <msfredb7@gmail.com>
2018-02-28 17:00:39 -0500

    Phase 2 de refactoring

---
+++ OceanEmpire/Assets/Game/Scripts/Units/Player Projectiles/Harpoon/Harpoon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harpoon : Projectile
+{
+    public float killAfter = 5;
+    private float deathTimer;
+    public SpriteRenderer harpoonSpriteRenderer;
+
+    protected override void Start()
+    {
+        base.Start();
+        deathTimer = killAfter;
+    }
+
+    private void Update()
+    {
+        if (deathTimer > 0)
+        {
+            deathTimer -= Time.deltaTime;
+            if (deathTimer <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+
+    public void OnUnitHit(ColliderInfo info, Collider2D collider)
+    {
+        var harpoonBlocker = info.Parent.GetComponent<HarpoonBlocker>();
+
+        if (harpoonBlocker != null)
+        {
+            harpoonBlocker.HarpoonHit();
+            Kill();
+        }
+    }
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+        Destroy(gameObject);
+    }
+
+    void SetHarpoonSprite(Sprite hSprite)
+    {
+        harpoonSpriteRenderer.sprite = hSprite;
+    }
+}
---
+++ OceanEmpire/Assets/Game/Scripts/Units/Sous-Marin/SubmarinParts.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarinParts : MonoBehaviour
+{
+    public Thruster Thruster { private set; get; }
+    public HarpoonThrower HarpoonThrower { private set; get; }
+    public GazTank GazTank { private set; get; }
+
+    [Header("Categories"), SerializeField] private ThrusterCategory thrusterCategory;
+    [SerializeField] private HarpoonThrowerCategory harpoonThrowerCategory;
+    [SerializeField] private GazTankCategory gazTankCategory;
+
+    [Header("Default upgrades"), SerializeField] private bool useDefaults = false;
+    [SerializeField] private ThrusterDescBuilder prebuiltThruster;
+    [SerializeField] private HarpoonThrowerDescBuilder prebuiltHarpoon;
+    [SerializeField] private GazTankDescBuilder prebuiltGazTank;
+
+    void Start()
+    {
+        GazTankDescription GTD = null;
+        ThrusterDescription TD = null;
+        HarpoonThrowerDescription HTD = null;
+        if (useDefaults)
+        {
+            TD = prebuiltThruster.BuildUpgradeDescription();
+            HTD = prebuiltHarpoon.BuildUpgradeDescription();
+            GTD = prebuiltGazTank.BuildUpgradeDescription();
+        }
+        else
+        {
+            TD = thrusterCategory.GetCurrentDescription();
+            HTD = harpoonThrowerCategory.GetCurrentDescription();
+            GTD = gazTankCategory.GetCurrentDescription();
+        }
+
+        GazTank = new GazTank(GTD);
+        Thruster = new Thruster(TD);
+        HarpoonThrower = new HarpoonThrower(HTD);
+
+        ApplyFuelTank();
+        ApplyHarpoonThrower();
+        ApplyThruster();
+    }
+
+    void ApplyThruster()
+    {
+        var movement = GetComponent<SubmarineMovement>();
+        if (movement != null)
+        {
+            movement.maximumSpeed = Thruster.Description.GetSpeed();
+            movement.accelerationRate = Thruster.Description.GetAcceleration();
+        }
+    }
+
+    void ApplyFuelTank()
+    {
+    }
+
+    void ApplyHarpoonThrower()
+    {
+        var slingshotControl = GetComponent<SlingshotControl>();
+
+        if (slingshotControl != null)
+        {
+            HarpoonThrowerDescription desc = HarpoonThrower.Description;
+            slingshotControl.SetHarpoonVisuals(desc.GetCanonSprite(), desc.GetPullSprite(), desc.GetHarpoonSprite());
+            slingshotControl.SetHarpoonSpeed(desc.GetHarpoonSpeed());
+            slingshotControl.HarpoonCooldown = desc.GetCooldown();
+            slingshotControl.HarpoonCount = 1;
+        }
+    }
+
+    private void Update()
+    {
+        if (GazTank != null)
+            GazTank.UpdateTimer();
+    }
+}
