ec5b266d5007c7ba489cb12f95a604ba2960cba3
Frédéric Bessette <msfredb7@gmail.com>
2018-03-15 15:24:45 -0400

    Hide/Show Inheritence fix

--- OceanEmpire/Assets/Plugins/CCC/Attributes/Editor/HideShowBaseDrawer.cs
+++ OceanEmpire/Assets/Plugins/CCC/Attributes/Editor/HideShowBaseDrawer.cs
@@ -36,63 +36,79 @@ public abstract class HideShowBaseDrawer : PropertyDrawer
             return DefaultMemberValue;
 
         UnityEngine.Object instance = so.targetObject;
+        //Type t = instance.GetType();
+        //while (t != null)
+        //{
+        //    Debug.Log(t.Name);
+        //    t = t.BaseType;
+        //}
+
         Type type = instance.GetType();
         HideShowBaseAttribute.Type memberType = GetMemberType();
         string name = GetMemberName();
 
+        while (type != null)
+        {
+            switch (memberType)
+            {
+                case HideShowBaseAttribute.Type.Property:
+                    PropertyInfo propInfo = type.GetProperty(name);
+                    if (propInfo == null)
+                        break;
+                    else if (propInfo.PropertyType != typeof(bool))
+                    {
+                        Debug.LogWarning("(Show/Hide Attribute) The property named \"" + name + "\" is not of type bool.");
+                        return DefaultMemberValue;
+                    }
+                    else
+                    {
+                        return (bool)propInfo.GetValue(instance, null);
+                    }
+                case HideShowBaseAttribute.Type.Method:
+                    MethodInfo methodInfo = type.GetMethod(name);
+                    if (methodInfo == null)
+                        break;
+                    else if (methodInfo.ReturnType != typeof(bool))
+                    {
+                        Debug.LogWarning("(Show/Hide Attribute) The method named \"" + name + "\" does not return a bool.");
+                        return DefaultMemberValue;
+                    }
+                    else
+                    {
+                        return (bool)methodInfo.Invoke(instance, null);
+                    }
+                case HideShowBaseAttribute.Type.Field:
+                    FieldInfo fieldInfo = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                    if (fieldInfo == null)
+                        break;
+                    else if (fieldInfo.FieldType != typeof(bool))
+                    {
+                        Debug.LogWarning("(Show/Hide Attribute) The field named \"" + name + "\" is not of type bool.");
+                        return DefaultMemberValue;
+                    }
+                    else
+                    {
+                        return (bool)fieldInfo.GetValue(instance);
+                    }
+                default:
+                    Debug.LogWarning("Error in Show/Hide property drawing.");
+                    return DefaultMemberValue;
+            }
+            type = type.BaseType;
+        }
+
         switch (memberType)
         {
-            case HideShowBaseAttribute.Type.Property:
-                PropertyInfo propInfo = type.GetProperty(name);
-                if (propInfo == null)
-                {
-                    Debug.LogWarning("(Show/Hide Attribute) Failed to get the property named \"" + name + "\".");
-                    return DefaultMemberValue;
-                }
-                else if (propInfo.PropertyType != typeof(bool))
-                {
-                    Debug.LogWarning("(Show/Hide Attribute) The property named \"" + name + "\" is not of type bool.");
-                    return DefaultMemberValue;
-                }
-                else
-                {
-                    return (bool)propInfo.GetValue(instance, null);
-                }
-            case HideShowBaseAttribute.Type.Method:
-                MethodInfo methodInfo = type.GetMethod(name);
-                if (methodInfo == null)
-                {
-                    Debug.LogWarning("(Show/Hide Attribute) Failed to get the method named \"" + name + "\".");
-                    return DefaultMemberValue;
-                }
-                else if (methodInfo.ReturnType != typeof(bool))
-                {
-                    Debug.LogWarning("(Show/Hide Attribute) The method named \"" + name + "\" does not return a bool.");
-                    return DefaultMemberValue;
-                }
-                else
-                {
-                    return (bool)methodInfo.Invoke(instance, null);
-                }
             case HideShowBaseAttribute.Type.Field:
-                FieldInfo fieldInfo = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-                if (fieldInfo == null)
-                {
-                    Debug.LogWarning("(Show/Hide Attribute) Failed to get the field named \"" + name + "\".");
-                    return DefaultMemberValue;
-                }
-                else if (fieldInfo.FieldType != typeof(bool))
-                {
-                    Debug.LogWarning("(Show/Hide Attribute) The field named \"" + name + "\" is not of type bool.");
-                    return DefaultMemberValue;
-                }
-                else
-                {
-                    return (bool)fieldInfo.GetValue(instance);
-                }
-            default:
-                Debug.LogWarning("Error in Show/Hide property drawing.");
-                return DefaultMemberValue;
+                Debug.LogWarning("(Show/Hide Attribute) Failed to get the field named \"" + name + "\".");
+                break;
+            case HideShowBaseAttribute.Type.Property:
+                Debug.LogWarning("(Show/Hide Attribute) Failed to get the property named \"" + name + "\".");
+                break;
+            case HideShowBaseAttribute.Type.Method:
+                Debug.LogWarning("(Show/Hide Attribute) Failed to get the method named \"" + name + "\".");
+                break;
         }
+        return DefaultMemberValue;
     }
 }
