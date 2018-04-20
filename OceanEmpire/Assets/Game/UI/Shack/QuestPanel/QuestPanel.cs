850f93f7a1029b78af82c7c9efc31e97550e2c2a
Frédéric Bessette <msfredb7@gmail.com>
2018-04-20 14:28:09 -0400

    Quest panel intro animation

--- OceanEmpire/Assets/Game/UI/Shack/QuestPanel/QuestPanel.cs
+++ OceanEmpire/Assets/Game/UI/Shack/QuestPanel/QuestPanel.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Questing;
+using DG.Tweening;
 
 public class QuestPanel : MonoBehaviour
 {
+    [Header("Entries Managment")]
     public QuestPanelEntry entryPrefab;
     public List<QuestPanelEntry> entries = new List<QuestPanelEntry>();
     public RectTransform entriesContainer;
+
+    [Header("Animation")]
+    public CanvasGroup canvasGroup;
+    public Vector2 arrivalDelta;
+    public float arrivalDelay;
+    public Ease arrivalEase = Ease.OutSine;
+    public float moveDuration = 1.5f;
+    public float fadeInDuration = 1.5f;
+    public bool showOnStart = true;
 
     private void Awake()
     {
@@ -16,6 +27,33 @@ public class QuestPanel : MonoBehaviour
             UpdateContent();
             QuestManager.Instance.OnListChange += UpdateContent;
         });
+    }
+
+    private void Start()
+    {
+        if (showOnStart)
+            Show();
+        else
+            HideInstant();
+    }
+
+    void Show()
+    {
+        var rectTr = GetComponent<RectTransform>();
+        var destination = rectTr.anchoredPosition;
+        var sq = DOTween.Sequence();
+
+        canvasGroup.alpha = 0;
+        rectTr.anchoredPosition += arrivalDelta;
+
+        sq.AppendInterval(arrivalDelay);
+        sq.Append(rectTr.DOAnchorPos(destination, moveDuration).SetEase(arrivalEase));
+        sq.Join(canvasGroup.DOFade(1, fadeInDuration));
+    }
+
+    void HideInstant()
+    {
+        gameObject.SetActive(false);
     }
 
     private void OnDestroy()
@@ -27,7 +65,7 @@ public class QuestPanel : MonoBehaviour
     public void UpdateContent()
     {
         QuestManager questManager = QuestManager.Instance;
-        if(questManager == null)
+        if (questManager == null)
         {
             Debug.LogError("No quest manager");
             return;
