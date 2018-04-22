1111d71dbad1c6def4719e83cb6d7655f2e5a49c
Frédéric Bessette <msfredb7@gmail.com>
2018-04-22 02:54:01 -0400

    QuestPanel animation

--- OceanEmpire/Assets/Game/UI/Shack/QuestPanel/QuestPanel.cs
+++ OceanEmpire/Assets/Game/UI/Shack/QuestPanel/QuestPanel.cs
@@ -11,7 +11,7 @@ public class QuestPanel : MonoBehaviour
     public List<QuestPanelEntry> entries = new List<QuestPanelEntry>();
     public RectTransform entriesContainer;
 
-    [Header("Animation")]
+    [Header("Arrival Animation")]
     public CanvasGroup canvasGroup;
     public Vector2 arrivalDelta;
     public float arrivalDelay;
@@ -20,8 +20,22 @@ public class QuestPanel : MonoBehaviour
     public float fadeInDuration = 1.5f;
     public bool showOnStart = true;
 
+    [Header("Offer Next Map Animation")]
+    public Vector2 onm_targetSizeDelta;
+    public float onm_sizeDuration;
+    public Ease onm_sizeEase;
+    public RectTransform onm_button;
+    public float onm_buttonScaleDuration;
+    public Ease onm_buttonScaleEase;
+
+    private Tween ongoingShowAnimation;
+    private Vector2 normalAnchoredPosition;
+    private bool isShown = false;
+
     private void Awake()
     {
+        normalAnchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
         PersistentLoader.LoadIfNotLoaded(() =>
         {
             UpdateContent();
@@ -31,28 +45,84 @@ public class QuestPanel : MonoBehaviour
 
     private void Start()
     {
+        onm_button.gameObject.SetActive(false);
+
         if (showOnStart)
-            Show();
+        {
+            if (!isShown)
+                Show();
+        }
         else
             HideInstant();
     }
 
     void Show()
     {
+        this.DOKill();
+
+        isShown = true;
         var rectTr = GetComponent<RectTransform>();
-        var destination = rectTr.anchoredPosition;
         var sq = DOTween.Sequence();
 
         canvasGroup.alpha = 0;
-        rectTr.anchoredPosition += arrivalDelta;
+        rectTr.anchoredPosition = normalAnchoredPosition + arrivalDelta;
 
         sq.AppendInterval(arrivalDelay);
-        sq.Append(rectTr.DOAnchorPos(destination, moveDuration).SetEase(arrivalEase));
+        sq.Append(rectTr.DOAnchorPos(normalAnchoredPosition, moveDuration).SetEase(arrivalEase));
         sq.Join(canvasGroup.DOFade(1, fadeInDuration));
+        sq.SetId(this);
+        ongoingShowAnimation = sq;
+    }
+
+    void OfferNextMap()
+    {
+        if (this == null || onm_button == null)
+            return;
+
+        this.DOKill();
+
+        TweenCallback onAllEntriesGone = () =>
+        {
+            if (this == null || onm_button == null)
+                return;
+
+            var sq = DOTween.Sequence();
+            onm_button.localScale = Vector3.one * 0.5f;
+
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.interactable = true;
+            // Resize
+            sq.Append(GetComponent<RectTransform>().DOSizeDelta(onm_targetSizeDelta, onm_sizeDuration).SetEase(onm_sizeEase));
+
+            // Enable button
+            sq.AppendCallback(() => onm_button.gameObject.SetActive(true));
+
+            // Scale up button
+            var scaleUp = onm_button.DOScale(1, onm_buttonScaleDuration).SetEase(onm_buttonScaleEase);
+            scaleUp.easeOvershootOrAmplitude = 0;
+            sq.Append(scaleUp);
+
+            sq.SetId(this);
+        };
+
+        if (entries.Count > 0)
+        {
+            entries[0].FlashAwayAnimation(onAllEntriesGone);
+            for (int i = 1; i < entries.Count; i++)
+            {
+                entries[i].FlashAwayAnimation();
+            }
+        }
+        else
+        {
+            onAllEntriesGone();
+        }
     }
 
     void HideInstant()
     {
+        this.DOKill();
+        isShown = false;
         gameObject.SetActive(false);
     }
 
@@ -85,6 +155,35 @@ public class QuestPanel : MonoBehaviour
         {
             entries[i].gameObject.SetActive(false);
         }
+
+
+        // Every quest completed !
+        if (IsEveryOngoingQuestCompleted())
+        {
+            if (isShown)
+            {
+                if (ongoingShowAnimation != null && ongoingShowAnimation.IsActive())
+                    ongoingShowAnimation.onComplete = OfferNextMap;
+                else
+                    OfferNextMap();
+            }
+            else
+            {
+                Show();
+                ongoingShowAnimation.onComplete = OfferNextMap;
+            }
+
+        }
+    }
+
+    private bool IsEveryOngoingQuestCompleted()
+    {
+        foreach (var quest in QuestManager.Instance.ongoingQuests)
+        {
+            if (quest.state != QuestState.Completed)
+                return false;
+        }
+        return true;
     }
 
     private QuestPanelEntry GetEntry(int i)
