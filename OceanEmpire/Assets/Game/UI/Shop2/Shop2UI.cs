f2017991cbaf7008995354396107bf09209c770d
Frédéric Bessette <msfredb7@gmail.com>
2018-04-26 01:53:54 -0400

    New Shop Started

--- OceanEmpire/Assets/Game/UI/Shop2/Shop2UI.cs
+++ OceanEmpire/Assets/Game/UI/Shop2/Shop2UI.cs
@@ -2,61 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shop2UI : MonoBehaviour {
+public class Shop2UI : MonoBehaviour
+{
 
     public CategoryDisplay prefabCategoryDisplay;
     public GameObject shopCategoryLayout;
 
-    public List<ScriptableObject> categories;
-    private List<CategoryDisplay> categoryList;
+    //public List<ScriptableObject> categories;
+    //public List<Cater>
+    public List<CategoryDisplay> categoryList = new List<CategoryDisplay>();
 
-    private void OnValidate()
-    {
-        for (int i = 0; i < categories.Count; i++)
-        {
-            if (!(categories[i] is IShopDisplayable))
-            { 
-                categories.RemoveAt(i);
-                i--;
-                Debug.LogError("Les élements de categories doivent être IShopDisplayable!!!");
-            }
-        }
-    }
+    //private void OnValidate()
+    //{
+    //    for (int i = 0; i < categories.Count; i++)
+    //    {
+    //        if (!(categories[i] is IShopDisplayable))
+    //        {
+    //            categories.RemoveAt(i);
+    //            i--;
+    //            Debug.LogError("Les élements de categories doivent être IShopDisplayable!!!");
+    //        }
+    //    }
+    //}
 
-    // Use this for initialization
-    void Start () {
-        Init();
-    }
-
-    private void Init()
-    {
-        categoryList = new List<CategoryDisplay>();
-        UpdateDisplay();
-
-        //PlayerCurrency.CurrencyUpdate += UpdateDisplay;
-    }
+    //void Start()
+    //{
+    //    UpdateContent();
+    //}
 
 
-    void UpdateDisplay()
-    {
-        for (int i = 0; i < categoryList.Count; ++i)
-        {
-            if (categoryList[i] != null)
-                Destroy(categoryList[i].gameObject);
-        }
+    //void UpdateContent()
+    //{
+    //    for (int i = 0; i < categoryList.Count; ++i)
+    //    {
+    //        if (categoryList[i] != null)
+    //            Destroy(categoryList[i].gameObject);
+    //    }
 
 
-        categoryList.Clear();
-        int categoryCount = categories.Count;
+    //    categoryList.Clear();
+    //    int categoryCount = categories.Count;
 
-        for (int i = 0; i < categoryCount; ++i)
-            DisplayCategories(categories[i] as IShopDisplayable);
-    }
+    //    for (int i = 0; i < categoryCount; ++i)
+    //        DisplayCategories(categories[i] as IShopDisplayable);
+    //}
 
-    void DisplayCategories(IShopDisplayable itemDescription)
-    {
-        CategoryDisplay newCategory = Instantiate(prefabCategoryDisplay, shopCategoryLayout.transform);
-        newCategory.SetValues(itemDescription);
-            categoryList.Add(newCategory);        
-    }
+    //void DisplayCategories(IShopDisplayable itemDescription)
+    //{
+    //    CategoryDisplay newCategory = Instantiate(prefabCategoryDisplay, shopCategoryLayout.transform);
+    //    newCategory.SetValues(itemDescription);
+    //    categoryList.Add(newCategory);
+    //}
 }
