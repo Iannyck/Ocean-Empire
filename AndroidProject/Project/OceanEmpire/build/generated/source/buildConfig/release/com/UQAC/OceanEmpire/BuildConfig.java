f686a48d32646667bc51ea29650d0502fda985f0
Xeal <alexandre-blanchet94@hotmail.com>
2018-06-04 23:29:10 -0400

    push du dernier projet android avec son apk

--- AndroidProject/Project/OceanEmpire/build/generated/source/buildConfig/release/com/UQAC/OceanEmpire/BuildConfig.java
+++ AndroidProject/Project/OceanEmpire/build/generated/source/buildConfig/release/com/UQAC/OceanEmpire/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.UQAC.OceanEmpire";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10;
-  public static final String VERSION_NAME = "10";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "12";
 }
