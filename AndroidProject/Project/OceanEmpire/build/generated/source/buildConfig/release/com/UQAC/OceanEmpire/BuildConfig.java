45ea8c6d0538d451b802b2e2a19cfcb076f0931a
Xeal <alexandre-blanchet94@hotmail.com>
2018-06-11 21:44:21 -0400

    push final apk

--- AndroidProject/Project/OceanEmpire/build/generated/source/buildConfig/release/com/UQAC/OceanEmpire/BuildConfig.java
+++ AndroidProject/Project/OceanEmpire/build/generated/source/buildConfig/release/com/UQAC/OceanEmpire/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.UQAC.OceanEmpire";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "2";
+  public static final int VERSION_CODE = 14;
+  public static final String VERSION_NAME = "14";
 }
